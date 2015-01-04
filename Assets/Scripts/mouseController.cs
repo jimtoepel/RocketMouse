be909cd82187d5cc1f71c26b36d1107e50d5bbbf
jimtoepel <jim.toepel@gmail.com>
2015-01-03 18:23:54 -0800

    Added collision and particle effects

---
+++ Assets/Scripts/mouseController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseController : MonoBehaviour {
+
+public float jetpackForce = 75.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		bool jetpackActive = Input.GetButton ("Fire1");
+
+		if (jetpackActive) {
+
+			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+
+		}
+
+	}
+
+
+}
