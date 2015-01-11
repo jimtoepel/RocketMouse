a6ed29d8fef9a17add3db0cea35bc17de5c0e219
jimtoepel <jim.toepel@gmail.com>
2015-01-11 15:50:08 -0800

    Randomly generates objects

--- Assets/Scripts/GeneratorScript.cs
+++ Assets/Scripts/GeneratorScript.cs
@@ -5,10 +5,25 @@ using System.Collections.Generic;
 public class GeneratorScript : MonoBehaviour {
 
 
-
+//rooms
 	public GameObject[] availableRooms;
 	public List<GameObject> currentRooms;
 	private float screenWidthInPoints;
+
+
+//objects
+	public GameObject[] availableObjects;
+	public List<GameObject> objects;
+
+	public float objectsMinDistance = 5.0f;
+	public float objectsMaxDistance = 10.0f;
+
+	public float objectsMinY = -1.4f;
+	public float objectsMaxY = -1.4f;
+
+	public float objectsMinRotation = -45.0f;
+	public float objectsMaxRotation = 45.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +36,7 @@ public class GeneratorScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		GenerateRoomIfRequired ();
+		GenerateObjectsIfRequired ();
 
 	}
 
@@ -103,4 +119,54 @@ public class GeneratorScript : MonoBehaviour {
 		if (addRooms)
 			AddRoom (farthestRoomEndX);
 	}
+
+
+	void AddObject(float lastObjectX)
+	{
+		int randomIndex = Random.Range (0, availableObjects.Length);
+
+		GameObject obj = (GameObject)Instantiate (availableObjects [randomIndex]);
+
+		float objectPositionX = lastObjectX + Random.Range (objectsMinDistance, objectsMaxDistance);
+		float randomY = Random.Range (objectsMinY, objectsMaxY);
+		obj.transform.position = new Vector3 (objectPositionX, randomY, 0);
+
+		float rotation = Random.Range (objectsMinRotation, objectsMaxRotation);
+		obj.transform.rotation = Quaternion.Euler(Vector3.forward * rotation);
+
+		objects.Add (obj);
+
+	}
+
+
+	void GenerateObjectsIfRequired()
+	{
+		float playerX = transform.position.x;
+		float removeObjectsX = playerX - screenWidthInPoints;
+		float addObjectX = playerX + screenWidthInPoints;
+		float farthestObjectX = 0;
+
+		List<GameObject> objectsToRemove = new List<GameObject> ();
+
+		foreach (var obj in objects) 
+		{
+			float objX = obj.transform.position.x;
+
+			farthestObjectX = Mathf.Max (farthestObjectX, objX);
+
+			if (objX < removeObjectsX)
+				objectsToRemove.Add (obj);
+		}
+
+		foreach (var obj in objectsToRemove)
+		{
+			objects.Remove (obj);
+			DestroyObject(obj);
+
+		}
+
+		if (farthestObjectX < addObjectX)
+			AddObject (farthestObjectX);
+	}
+
 }
