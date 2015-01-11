6d1c5236868e47808c9e82d9f118d555796cb65c
jimtoepel <jim.toepel@gmail.com>
2015-01-11 14:08:44 -0800

    Added run animations and ground collision

--- Assets/Scripts/mouseController.cs
+++ Assets/Scripts/mouseController.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class mouseController : MonoBehaviour {
+public class MouseController : MonoBehaviour {
 
 public float jetpackForce = 75.0f;
 public float forwardMovementSpeed = 3.0f;
+public Transform groundCheckTransform;
+public bool grounded;
+public LayerMask groundCheckLayerMask;
+
+Animator animator;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -28,6 +34,19 @@ public float forwardMovementSpeed = 3.0f;
 		Vector2 newVelocity = rigidbody2D.velocity;
 		newVelocity.x = forwardMovementSpeed;
 		rigidbody2D.velocity = newVelocity;
+
+		UpdateGroundedStatus();
+
+	}
+
+	void UpdateGroundedStatus()
+	{
+		//1
+		grounded = Physics2D.OverlapCircle (groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+
+		//2
+		animator.SetBool ("grounded", grounded);
 
 	}
 
