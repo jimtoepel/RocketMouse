d8f2e4ad27db6fe20d48985c637ceaf3f9fa5076
jimtoepel <jim.toepel@gmail.com>
2015-01-18 13:56:50 -0800

    Start button works now

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame ()
+	{
+		Application.LoadLevel ("RocketMouse");
+		
+	}
+
+}
+
