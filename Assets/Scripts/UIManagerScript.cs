487c0afefc1606c6158998c3947ec7027adce21f
jimtoepel <jim.toepel@gmail.com>
2015-01-18 14:28:31 -0800

    Settings Button works

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
+	public Animator startButton;
+	public Animator settingsButton;
+
+
 	public void StartGame ()
 	{
 		Application.LoadLevel ("RocketMouse");
 		
+	}
+
+
+	public void OpenSettings()
+	{
+
+		startButton.SetBool ("isHidden", true);
+		settingsButton.SetBool ("isHidden", true);
+	
 	}
 
 }
