cfe1e0214e1e0b6d54e29911aed7e6878aa9be4b
jimtoepel <jim.toepel@gmail.com>
2015-01-18 21:10:04 -0800

    Functional sandwich menu
    But nothing in it does anything.

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,7 +6,18 @@ public class UIManagerScript : MonoBehaviour {
 	public Animator startButton;
 	public Animator settingsButton;
 	public Animator dialog;
+	public Animator contentPanel;
+	public Animator gearImage;
 
+	void Start()
+	{
+
+		RectTransform transform = contentPanel.gameObject.transform as RectTransform;
+		Vector2 position = transform.anchoredPosition;
+		position.y -= transform.rect.height;
+		transform.anchoredPosition = position; 
+
+	}
 
 	public void StartGame ()
 	{
@@ -32,6 +43,20 @@ public class UIManagerScript : MonoBehaviour {
 		startButton.SetBool ("isHidden", false);
 		settingsButton.SetBool ("isHidden", false);
 		dialog.SetBool ("isHidden", true);
+
+	}
+
+
+	public void ToggleMenu()
+	{
+
+		contentPanel.enabled = true;
+
+		bool isHidden = contentPanel.GetBool ("isHidden");
+		contentPanel.SetBool ("isHidden", !isHidden);
+
+		gearImage.enabled = true;
+		gearImage.SetBool ("isHidden", !isHidden);
 
 	}
 
